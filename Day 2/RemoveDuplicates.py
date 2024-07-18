e18137a5c51725d4b3ea7aef8a6fbf6315c0bfab
thisisvishnu567 <117495092+thisisvishnu567@users.noreply.github.com>
2024-07-18 07:39:33 +0530

    Progress(1/5)

---
+++ Day 2/RemoveDuplicates.py
@@ -0,0 +1,11 @@
+from typing import List
+
+def solution(nums : List[int]) -> int:
+    i = 1
+    for j in range(1,len(nums)):
+        if nums[j] != nums[j-1]:
+            nums[i] = nums[j]
+            i+=1
+    return i
+
+nums = [0,0,1,1,1,2,2,3,3,3,]
