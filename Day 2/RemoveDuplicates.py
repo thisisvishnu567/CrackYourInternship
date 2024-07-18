a4c659135e23020e009196f1dd23f61903300e22
thisisvishnu567 <117495092+thisisvishnu567@users.noreply.github.com>
2024-07-18 07:56:03 +0530

    Progress(1/5)

--- Day 2/RemoveDuplicates.py
+++ Day 2/RemoveDuplicates.py
@@ -8,4 +8,8 @@ def solution(nums : List[int]) -> int:
             i+=1
     return i
 
-nums = [0,0,1,1,1,2,2,3,3,3,]
+
+nums = [0,0,1,1,1,2,2,3,3,3,4]
+solution(nums)
+for i in nums:
+    print(nums[i] , end=" ")
